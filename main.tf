4093432128d81ba78db73651c786f3709508905b
briane-ship-it <briane@spacelift.io>
2026-07-31 13:28:56 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ resource "aws_s3_bucket" "orbit_storage" {
   bucket_prefix = "orbit-storage-"
 
   tags = {
-    name      = "Orbit Labs Storage"
-    managedBy = "Spacelift"
-    mission   = "First Launch"
-    project   = "Orbit-labs"
-  }
+  name        = "Orbit Labs Storage"
+  managedBy   = "Spacelift"
+  mission     = "First Launch"
+  project     = "Orbit-labs"
+  environment = "demo"
 }
 
 terraform {
